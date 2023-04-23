6d3e4d3f08d54a955c7ce243b303d3376a0dfc28
mizonov-iv <mizonov.iv@gmail.com>
2023-04-23 17:51:05 +0500

    Slider first var

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,18 +1,79 @@
 <template>
-  <div class="slider">
+  <div class="wrapper">
+    <div
+        class="slider"
+        :style="{'margin-top': '-' + (100 * currentSlideIndex) + '%'}"
+    >
+      <SliderItem
+          v-for="item in slides"
+          :key="item.id"
+          :item="item"
+      />
+    </div>
 
   </div>
+  <button @click="nextSlide">Next</button>
 </template>
 
 <script>
 import SliderItem from "./SliderItem.vue";
 export default {
   components: {SliderItem},
-  props: ['slides']
-
+  props: {
+    slides: {
+      type: Array,
+      default: () => []
+    },
+    interval: {
+      type: Number,
+      default: 0
+    }
+  },
+  data() {
+    return {
+      currentSlideIndex: 0
+    }
+  },
+  methods: {
+    prevSlide() {
+      if(this.currentSlideIndex > 0) {
+        this.currentSlideIndex--
+      }
+    },
+    nextSlide() {
+      if (this.currentSlideIndex >= this.slides.length - 1) {
+        this.currentSlideIndex = 0
+      } else {
+        this.currentSlideIndex++
+      }
+    }
+  },
+  mounted() {
+    if(this.interval > 0) {
+      let vm = this
+      setInterval(function () {
+        vm.nextSlide()
+      }, vm.interval)
+    }
+  }
 }
 </script>
 
 <style scoped>
+
+.wrapper {
+  border: 2px solid black;
+  max-width: 400px;
+  margin: 0 auto;
+  overflow: hidden;
+  height: 100vh;
+}
+
+.slider {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  transition: all ease 0.5s;
+}
 
 </style>
--- src/components/SliderItem.vue
+++ src/components/SliderItem.vue
@@ -1,15 +1,30 @@
 <template>
   <div class="slider-item">
-
+    <img class="item-img" :src="item.img_url" alt="">
   </div>
 </template>
 
 <script>
 export default {
+  props: {
+    item: {
+      type: Object,
+      default: () => {}
+    }
+  }
 
 }
 </script>
 
 <style scoped>
+
+.item-img {
+  width: 400px;
+}
+
+.slider-item {
+  width: 100%;
+  height: 280px;
+}
 
 </style>
