74d2e4c4096f5b1b99677225742cd6b4ecf6c933
mizonov <mizonov.iv@gmail.com>
2023-06-15 12:00:30 +0500

    navbar icons refactor

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,13 @@
 <template>
   <Navbar/>
   <RouterView/>
-<!--  <Slider-->
-<!--      :slides="sliderItems"-->
-<!--      :interval="3000"-->
-<!--  />-->
 </template>
 
 <script>
-import Slider from "./components/Slider.vue";
-import SliderItem from "./components/SliderItem.vue";
 import Navbar from "./components/Navbar.vue";
 export default {
-  components: {SliderItem, Slider, Navbar},
+  components: {Navbar},
   data()  {
-    return {
-      sliderItems: [
-        {
-          title: "First Slide",
-          img_url: "/img/1.jpg"
-        },
-        {
-          title: "Second Slide",
-          img_url: "/img/2.jpg"
-        },
-        {
-          title: "Third Slide",
-          img_url: "/img/3.jpg"
-        },
-      ]
-    }
   },
 }
 </script>
